49705b94a004606497051b80857c380210e87f07
Carlos Vinicius <carlosjp@hotmail.co.jp>
2016-03-08 19:40:47 -0300

    ---

--- functions.base.js
+++ functions.base.js
@@ -2,34 +2,27 @@
 * Funções base
 */
 "function"!==typeof String.prototype.trim&&(String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g,"")});
-"function"!==typeof String.prototype.replaceSpecialChars&&(String.prototype.replaceSpecialChars=function(){var b={"\u00e7":"c","\u00e6":"ae","\u0153":"oe","\u00e1":"a","\u00e9":"e","\u00ed":"i","\u00f3":"o","\u00fa":"u","\u00e0":"a","\u00e8":"e","\u00ec":"i","\u00f2":"o","\u00f9":"u","\u00e4":"a","\u00eb":"e","\u00ef":"i","\u00f6":"o","\u00fc":"u","\u00ff":"y","\u00e2":"a","\u00ea":"e","\u00ee":"i","\u00f4":"o","\u00fb":"u","\u00e5":"a","\u00e3":"a","\u00f8":"o","\u00f5":"o",u:"u","\u00c1":"A","\u00c9":"E", "\u00cd":"I","\u00d3":"O","\u00da":"U","\u00ca":"E","\u00d4":"O","\u00dc":"U","\u00c3":"A","\u00d5":"O","\u00c0":"A","\u00c7":"C"};return this.replace(/[\u00e0-\u00fa]/ig,function(a){return"undefined"!=typeof b[a]?b[a]:a})});
-Array.prototype.indexOf||(Array.prototype.indexOf=function(d,e){var a;if(null==this)throw new TypeError('"this" is null or not defined');var c=Object(this),b=c.length>>>0;if(0===b)return-1;a=+e||0;Infinity===Math.abs(a)&&(a=0);if(a>=b)return-1;for(a=Math.max(0<=a?a:b-Math.abs(a),0);a<b;){if(a in c&&c[a]===d)return a;a++}return-1});
+"function"!==typeof String.prototype.replaceSpecialChars&&(String.prototype.replaceSpecialChars=function(){var b={"\u00e7": "c","\u00e6": "ae","\u0153": "oe","\u00e1": "a","\u00e9": "e","\u00ed": "i","\u00f3": "o","\u00fa": "u","\u00e0": "a","\u00e8": "e","\u00ec": "i","\u00f2": "o","\u00f9": "u","\u00e4": "a","\u00eb": "e","\u00ef": "i","\u00f6": "o","\u00fc": "u","\u00ff": "y","\u00e2": "a","\u00ea": "e","\u00ee": "i","\u00f4": "o","\u00fb": "u","\u00e5": "a","\u00e3": "a","\u00f8": "o","\u00f5": "o",u: "u","\u00c1": "A","\u00c9": "E", "\u00cd": "I","\u00d3": "O","\u00da": "U","\u00ca": "E","\u00d4": "O","\u00dc": "U","\u00c3": "A","\u00d5": "O","\u00c0": "A","\u00c7": "C"};return this.replace(/[\u00e0-\u00fa]/ig,function(a){return"undefined"!=typeof b[a]?b[a]: a})});
+Array.prototype.indexOf||(Array.prototype.indexOf=function(d,e){var a;if(null==this)throw new TypeError('"this" is null or not defined');var c=Object(this),b=c.length>>>0;if(0===b)return-1;a=+e||0;Infinity===Math.abs(a)&&(a=0);if(a>=b)return-1;for(a=Math.max(0<=a?a: b-Math.abs(a),0);a<b;){if(a in c&&c[a]===d)return a;a++}return-1});
 
 try {
 	var Common = {
 		run: function() {},
 		init: function() {
-			Common.start();
-
-			// if (!Common.logged()) 
-			// 	Common.loginModal();
-			// else
-			// 	Common.ordersChart();
+			if (!Common.logged())
+				Common.loginModal();
+			else
+				Common.ordersChart();
 		},
 		ajaxStop: function() {},
 		windowOnload: function() {},
-		start:function() {
-			$.ajax({
-				url: "http://dashboardapi.quatrodigital.com.br/start",
-				dataType: "json",
-				success:function(data) {
-					$.cookie('PHPSESSID' , data.PHPSESSID , { path: '/' });
-				}
-			})
-		},
-		logged:function() {
-			if ($.cookie('PHPSESSID')) 
-				return true;
+		logged: function() {
+			var token = $.cookie('qdToken');
+			if (!token)
+				return false;
+
+			$.ajaxSetup({headers: { 'x-qd-auth': token } });
+			window._QD_qd_auth = token;
 			return false;
 		},
 		ordersChart: function() {
@@ -66,102 +59,86 @@ try {
 				var canvas = document.getElementById("orders-chart");
 				var myNewChart = new Chart(canvas.getContext("2d")).Line(chartData, {
 					tooltipTemplate: "<%if (label){%><%=label%>: s<%}%>b<%= value %>s",
-					legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+					legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color: <%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
 					responsive: true
 				});
 
 				$(canvas).addClass("loaded").before('Período: ' + data.startDate + ' até ' + data.endDate + '');
 			});
-		}, 
+		},
 		loginModal: function() {
-			var elemModal = $('.modal-qd-v1').clone().appendTo(document.body);
-			elemModal.removeClass('modal-qd-v1'); 
+			var elemModal = $('.qd-modal-base').clone().removeClass("qd-modal-base").appendTo(document.body);
 			elemModal.find('.modal-title').text('Informe seu e-mail');
-			elemModal.find('.modal-body').html('<form class="login"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="email">E-mail:</label> <input type="email" class="form-control" id="email" name="email" placeholder="E-mail" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-login">Login</button> </form>');
-			elemModal.modal({backdrop: 'static', keyboard: false });
-			elemModal.on('hidden.bs.modal', function () { elemModal.remove(); });
+			var form = $('<form class="login"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="email">E-mail: </label> <input type="email" class="form-control" id="email" name="email" placeholder="E-mail" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-login">Login</button> </form>').appendTo(elemModal.find('.modal-body').empty());
 
-			elemModal.find('form.login').on('submit', function(e){
+			elemModal.modal({backdrop: 'static', keyboard: false });
+			elemModal.on('hidden.bs.modal', function() { elemModal.remove(); });
+
+			form.on('submit', function(e){
 				e.preventDefault();
 
+				form.find(".request-message").remove();
+				form.append('<div class="pull-right request-message"> <span class="label label-warning">Aguarde, estamos processando os dados</span> </div>');
 				$.ajax({
 					type: 'POST',
-					url : 'http://dashboardapi.quatrodigital.com.br/get-token',
-					data: {
-						email:elemModal.find('input#email').val()
-					},
-					beforeSend:function() {
-						elemModal.find('.modal-body form .pull-right').remove();	
-						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-warning">Aguarde, estamos processando os dados</span> </div>');
-					},
-					success:function(data) {
-						$.ajaxSetup({
-							headers: { 'x-qd-auth': data.xQdAuth }
-						});
+					url : '//dashboardapi.quatrodigital.com.br/get-token',
+					data: {email: elemModal.find('input#email').val() },
+					success: function(data) {
+						$.ajaxSetup({headers: { 'x-qd-auth': data.xQdAuth } });
+						window._QD_qd_auth = data.xQdAuth;
 
-						$.cookie('tmp_qd', data.xQdAuth, { expires: 7 });
-
-						elemModal.find('.modal-body form .pull-right').remove();	
+						elemModal.find('.modal-body form .pull-right').remove();
 						if (data.success) {
 							elemModal.modal('hide');
 							Common.checkToken();
-						} else {
+						} else
 							elemModal.find('.modal-body form form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel efetuar o login </span> </div>');
-						}
 					},
 					error: function(data){
-						elemModal.find('.modal-body form .pull-right').remove();	
-						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel efetuar o login </span> </div>');
-					}
-				});
-			}); 
-		},
-		checkToken: function(qdToken) { 
-			var elemModal = $('.modal-qd-v1').clone().appendTo(document.body);
-			elemModal.removeClass('modal-qd-v1');
-			elemModal.find('.modal-title').text('Informe a chave de acesso');
-			elemModal.find('.modal-body').html('<form class="checkToken"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="email">Chave de acesso com 4 dígitos:</label> <input type="tel" class="form-control" id="token" name="token" placeholder="Token" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-validate-token">validar</button> </form>');
-			elemModal.modal({backdrop: 'static', keyboard: false });
-			elemModal.on('hidden.bs.modal', function () { elemModal.remove(); });
-
-			elemModal.find('form.checkToken').on('submit', function(e){
-				e.preventDefault();
-				$.ajax({
-					type: 'GET',
-					url : 'http://dashboardapi.quatrodigital.com.br/token-validate',
-					data: {
-						token: elemModal.find('input#token').val()
-					},
-					beforeSend:function() {
-						elemModal.find('.modal-body form .pull-right').remove();	
-						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-warning">Aguarde, estamos processando os dados</span> </div>');
-					},
-					success:function(data) {
-						elemModal.find('.modal-body form .pull-right').remove();	
-						if (data.success) {						
-							if (!data.hasStores) {
-								elemModal.modal('hide');
-								Common.messageUserLogged(qdToken);
-							}
-							else {
-								window.location.reload();
-								$.cookie('x-qd-auth', $.cookie('tmp_qd'));
-								$.removeCookie('tmp_qd');
-							}
-						} else {
-							$.removeCookie('qdToken');
-							elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
-						}
-					},
-					error: function(data){
-						console.log(data);
-						elemModal.find('.modal-body form .pull-right').remove();	
-						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
+						form.find(".request-message").remove();
+						form.append('<div class="pull-right"> <span class="label label-danger">Não foi possivel efetuar o login </span> </div>');
 					}
 				});
 			});
 		},
-		messageUserLogged: function(qdToken) { 
+		checkToken: function(qdToken) {
+			var elemModal = $('.qd-modal-base').clone().removeClass("qd-modal-base").appendTo(document.body);
+			elemModal.find('.modal-title').text('Informe a chave de acesso');
+			var form  = $('<form class="checkToken"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="email">Chave de acesso com 4 dígitos: </label> <input type="tel" class="form-control" id="token" name="token" placeholder="Token" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-validate-token">validar</button> </form>').appendTo(elemModal.find('.modal-body').empty());
+
+			elemModal.modal({backdrop: 'static', keyboard: false });
+			elemModal.on('hidden.bs.modal', function () { elemModal.remove(); });
+
+			form.on('submit', function(e){
+				e.preventDefault();
+
+				form.find(".request-message").remove();
+				form.append('<div class="pull-right request-message"> <span class="label label-warning">Aguarde, estamos processando os dados</span> </div>');
+				$.ajax({
+					url : 'http://dashboardapi.quatrodigital.com.br/token-validate',
+					data: {token: form.find('input#token').val()},
+					success: function(data) {
+						elemModal.find('.modal-body form .pull-right').remove();
+						if (data.success) {
+							if (!data.hasStores) {
+								elemModal.modal('hide');
+								Common.messageUserLogged(qdToken);
+							}
+							else
+								window.location.reload();
+							$.cookie('qdToken', window._QD_qd_auth, { path: "/" });
+						}
+						else
+							elemModal.find('.modal-body form').append('<div class="pull-right request-message"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
+					},
+					error: function(data){
+						form.find(".request-message").remove();
+						form.append('<div class="pull-right request-message"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
+					}
+				});
+			});
+		},
+		messageUserLogged: function(qdToken) {
 			var elemModal = $('.modal-qd-v1').clone().appendTo(document.body);
 			elemModal.removeClass('modal-qd-v1');
 			elemModal.find('.modal-title').text('Parabens!');
@@ -172,65 +149,58 @@ try {
 			elemModal.find('.btn-validate-token').on('click', function(){
 				elemModal.modal('hide');
 				Common.setStore(qdToken);
-			}); 
+			});
 		},
 		setStore: function(qdToken) {
-			console.log(qdToken);
 			var elemModal = $('.modal-qd-v1').clone().appendTo(document.body);
 			elemModal.removeClass('modal-qd-v1');
 			elemModal.find('.modal-title').text('Informe os dados da instituição');
-			elemModal.find('.modal-body').html('<form class="cadastro"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="account">Account</label> <input type="text" class="form-control" id="account" name="account" placeholder="Account" value=""> </div> </div> <div class="col-xs-12"> <div class="form-group"> <label for="key">Key</label> <input type="text" class="form-control" id="key" name="key" placeholder="Key" value=""> </div> </div> <div class="col-xs-12"> <div class="form-group"> <label for="token">Token</label> <input type="text" class="form-control" id="token" name="token" placeholder="Token" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-login">cadastrar</button> </form>'); 
+			elemModal.find('.modal-body').html('<form class="cadastro"> <div class="row"> <div class="col-xs-12"> <div class="form-group"> <label for="account">Account</label> <input type="text" class="form-control" id="account" name="account" placeholder="Account" value=""> </div> </div> <div class="col-xs-12"> <div class="form-group"> <label for="key">Key</label> <input type="text" class="form-control" id="key" name="key" placeholder="Key" value=""> </div> </div> <div class="col-xs-12"> <div class="form-group"> <label for="token">Token</label> <input type="text" class="form-control" id="token" name="token" placeholder="Token" value=""> </div> </div> </div> <button type="submit" class="btn btn-primary btn-login">cadastrar</button> </form>');
 			elemModal.modal({backdrop: 'static', keyboard: false });
 			elemModal.on('hidden.bs.modal', function () { elemModal.remove(); });
 
 			elemModal.find('form.cadastro').on('submit', function(e){
 				e.preventDefault();
 
-				$.ajax({ 
+				$.ajax({
 					type: 'POST',
 					url : 'http://dashboardapi.quatrodigital.com.br/set-store',
 					data: {
-						account:elemModal.find('input#account').val(),
-						key:elemModal.find('input#key').val(),
-						token:elemModal.find('input#token').val()
+						account: elemModal.find('input#account').val(),
+						key: elemModal.find('input#key').val(),
+						token: elemModal.find('input#token').val()
 					},
-					beforeSend:function(xhr) {
-						elemModal.find('.modal-body form .pull-right').remove();	
+					beforeSend: function(xhr) {
+						elemModal.find('.modal-body form .pull-right').remove();
 						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-warning">Aguarde, estamos processando os dados</span> </div>');
 					},
-					success:function(data) {
-						elemModal.find('.modal-body form .pull-right').remove();	
+					success: function(data) {
+						elemModal.find('.modal-body form .pull-right').remove();
 						if (data.success) {
 							elemModal.modal('hide');
 							Common.messageSetStoreSaved(qdToken);
-							$.cookie('x-qd-auth', $.cookie('tmp_qd'));
 							$.removeCookie('tmp_qd');
-						} else {
-							elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
 						}
+						else
+							elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
 					},
 					error: function(data){
-						console.log( data );
-						elemModal.find('.modal-body form .pull-right').remove();	
+						elemModal.find('.modal-body form .pull-right').remove();
 						elemModal.find('.modal-body form').append('<div class="pull-right"> <span class="label label-danger">Não foi possivel validar o token </span> </div>');
 					}
 				});
 			});
 		},
-		messageSetStoreSaved: function(qdToken) { 
+		messageSetStoreSaved: function(qdToken) {
 			var elemModal = $('.modal-qd-v1').clone().appendTo(document.body);
 			elemModal.removeClass('modal-qd-v1');
 			elemModal.find('.modal-title').text('Parabens!');
 			elemModal.find('.modal-body').html('Você completou a faze de login e cadastro de stores , ver relatório . <br /><br /><button type="button" class="btn btn-primary btn-fim">fim</button>');
 			elemModal.modal({backdrop: 'static', keyboard: false });
 			elemModal.on('hidden.bs.modal', function () { elemModal.remove(); });
-			$.cookie('qdToken', token, { path: "/" });
-			$.ajaxSetup({
-				headers: { 'x-qd-token': qdToken },
-			});
 			elemModal.find('.btn-fim').on('click', function(){
 				window.location.reload();
-			}); 
+			});
 		},
 	}
 }
@@ -251,7 +221,7 @@ try {
 		$(function() {
 			body = $(document.body);
 			Common.init();
-			
+
 			$(document).ajaxStop(ajaxStop);
 			$(window).load(windowLoad);
 			body.addClass('jsFullLoaded');
